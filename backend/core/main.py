ff7af39602a6ad0c309edf09b24b5771afe38f4c
Gaurav Chatterjee <Gaurav.Chatterjee@CapitalOne.com>
2017-06-29 23:21:11 -0400

    python

--- backend/core/main.py
+++ backend/core/main.py
@@ -3,6 +3,7 @@ from course import Course
 import urllib.request as ulr
 import ssl
 import parser
+import cmu_course_api
 
 PPC_URL = 'https://engineering.cmu.edu/education/undergraduate-programs/curriculum/general-education/people-places-culture.html'
 SDM_URL = 'https://engineering.cmu.edu/education/undergraduate-programs/curriculum/general-education/social-analysis.html'
@@ -24,7 +25,7 @@ def findClasses(soup):
     '''takes in soup of a page and returns a list of the classes'''
     pass
 
-def main():
+def foo():
     urls = [PPC_URL, SDM_URL, II_URL, WE_URL]
     for url in urls:
         html = getPage(url)
@@ -33,6 +34,10 @@ def main():
             numbers = parser.number(course.get_text())
             name = parser.name(course.get_text())
             # Doesn't do anything yet
+
+def main():
+    data = cmu_course_api.get_course_data('Fall 2017')
+    print(str(data)[:100])
 
 if __name__ == "__main__":
     main()
